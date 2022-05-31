9f7a15c7573f032439e395b90e7e7c3d0f9e4325
ViniciusFernandes <vnferna@gmail.com>
2022-05-31 05:57:31 -0300

    Correções no blog

--- _Blog/blog.php
+++ _Blog/blog.php
@@ -5,28 +5,14 @@
   $blog = new blog();
   $posts = $blog->buscarPosts();
   //
-  if(!empty($posts->error)){
-    if($posts->error->status == 403){
-      $html->mostraErro("Você não tem acesso a este conteudo, crie uma conta e tente novamente!");
-    }else{
-      $html->mostraErro("Erro ao carregar conteudo!");
-    }
-  }
-  //
   $htmlPosts = '';
   //
   foreach($posts->data AS $post){
     $htmlPosts .= $blog->geraPost($post);
   }
   //
-  if (isset($_SESSION['mensagem'])) {
-    $msg = $html->mostraMensagem($_SESSION['tipoMsg'], $_SESSION['mensagem']);
-    unset($_SESSION['mensagem'], $_SESSION['tipoMsg']);
-  }
-  //
   //Abre o arquivo html e Inclui mensagens e trechos php
   $html = $html->buscaHtml(pathinfo( __FILE__ ));
-  $html = str_replace("##Mensagem##", $msg, $html);
   $html = str_replace("##listaPosts##", $htmlPosts, $html);
   echo $html;
   exit;
--- _Blog/blog_post.php
+++ _Blog/blog_post.php
@@ -8,22 +8,8 @@
   //
   $post = $blog->buscaPost($idpost);
   //
-  if(!empty($post->error)){
-    if($post->error->status == 403){
-      $html->mostraErro("Você não tem acesso a este conteudo, crie uma conta e tente novamente!");
-    }else{
-      $html->mostraErro("Erro ao carregar conteudo!");
-    }
-  }
-  //
-  if (isset($_SESSION['mensagem'])) {
-    $msg = $html->mostraMensagem($_SESSION['tipoMsg'], $_SESSION['mensagem']);
-    unset($_SESSION['mensagem'], $_SESSION['tipoMsg']);
-  }
-  //
   //Abre o arquivo html e Inclui mensagens e trechos php
   $html = $html->buscaHtml(pathinfo( __FILE__ ));
-  $html = str_replace("##Mensagem##", $msg, $html);
   $html = str_replace("##tituloPost##", $post->data->attributes->title, $html);
   $html = str_replace("##conteudoPost##", $conversorMark->text($post->data->attributes->content), $html);
   $html = str_replace("##imgPost##", $post->data->attributes->banner->data->attributes->url, $html);
